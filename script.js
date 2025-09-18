e40f920f7f7b628a77f0572ee8c5b884e522b213
jakimiszka <jarek.jakimicha@gmail.com>
2025-09-18 23:59:32 +0200

    js

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-// const validate = () => {
-//     const name = document.getElementById("name").value;
-//     const email = document.getElementById("email").value;
-//     const message = document.getElementById("message").value;
-// }
+const first_name = document.querySelector('input[name="first_name"]');
+const last_name = document.querySelector('input[name="last_name"]');
+const email = document.querySelector('input[name="email"]');
+const general = document.querySelector('input[id="general"]');
+const support = document.querySelector('input[id="support"]');
+const message = document.querySelector('textarea[name="message"]');
+const submit = document.querySelector('button[type="submit"]');
+const fieldset = document.querySelector('fieldset');
 
+const validate = {
+    first_name: first_name.value,
+    last_name: last_name.value,
+    email: email.value,
+    general: general.checked,
+    support: support.checked,
+    message: message.value,
+
+    checkString: (string) => {
+        if (string === '') {
+            return false;
+        } else {
+            return true;
+        }
+
+        // prevent from XSS / SQL injection
+    },
+
+    checkEmail: (email) =>{
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(email).toLowerCase());
+    }
+}
+
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(fieldset.value);
+});
