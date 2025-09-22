dd0167496f9db1596a6ae731b7400990c70cf340
jakimiszka <jarek.jakimicha@gmail.com>
2025-09-22 20:48:02 +0200

    sanitizing input

--- script.js
+++ script.js
@@ -1,38 +1,143 @@
+// contrlos
 const first_name = document.querySelector('input[name="first_name"]');
 const last_name = document.querySelector('input[name="last_name"]');
 const email = document.querySelector('input[name="email"]');
-const general = document.querySelector('input[id="general"]');
-const support = document.querySelector('input[id="support"]');
+const query_types = document.querySelectorAll('input[name="query"]');
 const message = document.querySelector('textarea[name="message"]');
+const consent = document.querySelector('input[name="consent"]');
 const submit = document.querySelector('button[type="submit"]');
-const fieldset = document.querySelector('fieldset');
+const toast = document.querySelector('.toast');
+
+// error msgs
+const first_name_error = document.querySelector('.error.first_name');
+const last_name_error = document.querySelector('.error.last_name');
+const email_error = document.querySelector('.error.email');
+const query_error = document.querySelector('.error.query');
+const message_error = document.querySelector('.error.message');
+const consent_error = document.querySelector('.error.consent');
+
+const regex = /[^a-zA-Z]/g;
+const regex_message = /^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/g;
+const regex_email = /\S+@\S+\.\S+/;
+let results = [];
 
 const validate = {
-    first_name: first_name.value,
-    last_name: last_name.value,
-    email: email.value,
-    general: general.checked,
-    support: support.checked,
-    message: message.value,
-
     checkString: (string) => {
-        if (string === '') {
+        if (!string || regex.test(string)) {
             return false;
         } else {
             return true;
         }
+    },
 
-        // prevent from XSS / SQL injection
+    checkMessage: (string) => {
+        if (!string || regex_message.test(string)) {
+            return false;
+        } else {
+            return true;
+        }
     },
 
     checkEmail: (email) =>{
-        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return regex.test(String(email).toLowerCase());
+        const result = regex_email.test(email);
+        return !!result ? true : false;
     }
+}
+
+message.addEventListener('input', (event) => {
+  const input = event.target.value;
+  const sanitizedInput = sanitize(input);
+  event.target.value = sanitizedInput;
+});
+
+function sanitize(string) {
+  const map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#x27;',
+      "/": '&#x2F;',
+  };
+  const reg = /[&<>"'/]/ig;
+  return string.replace(reg, (match)=>(map[match]));
 }
 
 
 submit.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(fieldset.value);
+
+    results.push(validateString(first_name, first_name_error));
+    results.push(validateString(last_name, last_name_error));
+    results.push(validateEmail(email, email_error));
+    results.push(validateMessage(message, message_error));
+    results.push(consent.checked);
+    consent.checked ? consent_error.style.display = 'none' : consent_error.style.display = 'block';
+    let query_flag = false;
+    query_types.forEach((query_type) => {
+        if (query_type.checked) {
+            query_flag = true;
+        }
+    })
+    query_flag ? query_error.style.display = 'none' : query_error.style.display = 'block';
+    console.log(results);
+    submitForm(results);
 });
+
+function validateString(element, error_msg) {
+    if(validate.checkString(element.value)){ 
+        error_msg.style.display = 'none';
+        return true;
+    } else{
+        error_msg.style.display = 'block';
+        return false;
+    }
+}
+
+function validateMessage(element, error_msg) {
+    if(validate.checkMessage(element.value)){ 
+        error_msg.style.display = 'none';
+        return true;
+    } else{
+        error_msg.style.display = 'block';
+        return false;
+    }
+}
+
+function validateEmail(element, error_msg) {
+    if(validate.checkEmail(element.value)){
+         error_msg.style.display = 'none';
+         return true;
+    }else{
+        error_msg.style.display = 'block';
+        return false;
+    } 
+}
+
+
+function submitForm(results) {
+    if(results.indexOf(false) === -1){
+        toast.style.display = 'flex';
+        first_name.value = '';
+        last_name.value = '';
+        email.value = '';
+        message.value = '';
+        consent.checked = false;
+        query_types.forEach((query_type) => {
+            query_type.checked = false;
+        })
+
+        setTimeout(() => {
+            toast.style.display = 'none';
+        }, 2000);
+    }else{
+        toast.style.display = 'none';
+    }
+    results.length = 0;
+}
+
+function validateInput(event) {
+      var key = event.which || event.keyCode || 0;
+      return ((key >= 65 && key <= 92) || 
+              (key >= 97 && key <= 124) || key==32)
+    }
